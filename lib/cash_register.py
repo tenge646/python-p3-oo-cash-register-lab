111f4bce59c2b592c8fe459b313ccdb9d67f82bc
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-11-29 10:10:04 +0300

    done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
 
-# CashRegister class implementation
-
 class CashRegister:
-    def __init__(self, discount=0):
+  pass
+  def __init__(self, discount=0):
         self.discount = discount
         self.total = 0
         self.items = []
+        self.previous_transactions = []
 
-    def add_item(self, title, price, quantity=1):
+  def add_item(self, item, price, quantity=1):
         self.total += price * quantity
-        if quantity > 1:
-            self.items.append((title, price, quantity))
+        for _ in range(quantity):
+            self.items.append(item)
+        self.previous_transactions.append(
+            {"item": item, "quantity": quantity, "price": price}
+        )
+
+  def apply_discount(self):
+        if self.discount:
+            self.total = int(self.total * ((100 - self.discount) / 100))
+            print(f"After the discount, the total comes to ${self.total}.")
         else:
-            self.items.append((title, price))
+            print("There is no discount to apply.")
 
-    def apply_discount(self):
-     if self.discount > 0:
-        discount_amount = self.total * (self.discount / 100)
-        self.total -= discount_amount
-        print(f"After the discount, the total comes to ${self.total:.2f}.")
-     else:
-        print("There is no discount to apply.")
-
-
-
-
-    def void_last_transaction(self):
-        if self.items:
-            last_item = self.items.pop()
-            if len(last_item) > 2:
-                self.total -= last_item[1] * last_item[2]
-            else:
-                self.total -= last_item[1]
+  def void_last_transaction(self):
+        if not self.previous_transactions:
+            return "There are no transactions to void."
+        self.total -= (
+            self.previous_transactions[-1]["price"]
+            * self.previous_transactions[-1]["quantity"]
+        )
+        for _ in range(self.previous_transactions[-1]["quantity"]):
+            self.items.pop()
+        self.previous_transactions.pop()
